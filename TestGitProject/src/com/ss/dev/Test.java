74cf96d76653d06673af065e93326a614a2576d9
s.kharat <s.kharat@DSS-310.dsslp.com>
2014-12-23 15:51:44 +0530

    Test

---
+++ TestGitProject/src/com/ss/dev/Test.java
@@ -0,0 +1,17 @@
+package com.ss.dev;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+
+}
